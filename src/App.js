3f866acf4084f41b55da45e629be90f7d29d679a
Célio Guimarães <128413342+CelioGuimaraes117@users.noreply.github.com>
2023-09-29 20:33:41 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./component/header/header";
+import ShowNotes from "./component/showNotes/showNotes";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ShowNotes />
+    </>
+  );
+}
+
+export default App;
