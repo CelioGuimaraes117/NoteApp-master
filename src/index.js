cbf736b0533fef0e8333bfd08198b11b40aaebb9
Célio Guimarães <128413342+CelioGuimaraes117@users.noreply.github.com>
2023-10-02 01:25:39 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import "./style/main.css";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
+
+const port = process.env.PORT || 3001;
+
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
